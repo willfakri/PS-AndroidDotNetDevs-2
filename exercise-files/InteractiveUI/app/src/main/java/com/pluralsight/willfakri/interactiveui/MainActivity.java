878ad22f8c0285761e765325f1fa5f8905c6cdea
Willian Fakri <wfakri@ciandt.com>
2016-05-02 14:58:43 -0300

    Adicionado menu chamando as Activities

--- exercise-files/InteractiveUI/app/src/main/java/com/pluralsight/willfakri/interactiveui/MainActivity.java
+++ exercise-files/InteractiveUI/app/src/main/java/com/pluralsight/willfakri/interactiveui/MainActivity.java
@@ -1,17 +1,32 @@
 package com.pluralsight.willfakri.interactiveui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -26,9 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        switch (id){
-            case R.id.action_other:
-                onClickMenuOther(item);
+        switch (id) {
+            case R.id.activity2:
+                onClickMenuActivity2(item);
+                handled = true;
+                break;
+            case R.id.activity3:
+                onClickMenuActivity3(item);
                 handled = true;
                 break;
             case R.id.action_exit:
@@ -42,12 +61,59 @@ public class MainActivity extends AppCompatActivity {
         return handled;
     }
 
-    public void onClickMenuOther(MenuItem item){
-        Toast toast = Toast.makeText(this, "Booo!", Toast.LENGTH_LONG);
-        toast.show();
+    public void onClickMenuActivity2(MenuItem item) {
+//        Toast toast = Toast.makeText(this, "Booo!", Toast.LENGTH_LONG);
+//        toast.show();
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 
-    public void onClickMenuExit(MenuItem item){
+    public void onClickMenuActivity3(MenuItem item) {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+
+    public void onClickMenuExit(MenuItem item) {
         finish();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.pluralsight.willfakri.interactiveui/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.pluralsight.willfakri.interactiveui/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
